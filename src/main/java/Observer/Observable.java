89b5fa6d869ea1e5961fa6c3e044414308cc7bb8
Paulo Tapia <pmtapia@espol.edu.ec>
2024-11-28 22:38:02 -0500

    creacion de paquete observer

---
+++ src/main/java/Observer/Observable.java
@@ -0,0 +1,21 @@
+package Observer;
+
+public interface Observable {
+
+    
+} Observable {
+
+    public void agregarObservers(Observer observer){
+
+    }
+
+    public void eliminarObservers(Observer observer){
+
+    }
+
+    public void notifyObservers(Observer observer){
+
+    }
+
+    
+} 
---
+++ src/main/java/Observer/Observer.java
@@ -0,0 +1,9 @@
+package Observer;
+
+public interface Observer {
+
+    
+} Observer {
+    public void update(String message);
+    
+}
