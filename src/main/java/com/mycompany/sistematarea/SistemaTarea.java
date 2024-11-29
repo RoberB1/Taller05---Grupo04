3c636675e073e4cf260e162c59b296d13b507b15
Roberto C Barrios. <robauret@espol.edu.ec>
2024-11-28 21:10:29 -0500

    Proyecto de java

---
+++ src/main/java/com/mycompany/sistematarea/SistemaTarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistematarea;
+
+/**
+ *
+ * @author rb122
+ */
+public class SistemaTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
